41522ac1448168bfe6b7cb3abf4d6e4685cb1fd4
Fagner JB <fagnerjb@gmail.com>
2020-09-21 11:08:41 -0300

    Updates

---
+++ precache-manifest.8fe0de9240f46e04ae8fb655a17fb901.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e388bec488f62a439b79daeeb2da22",
+    "url": "/typing-react/index.html"
+  },
+  {
+    "revision": "2c2f32c9f56f18c6854b",
+    "url": "/typing-react/static/css/main.89b1f03b.chunk.css"
+  },
+  {
+    "revision": "eec0949e73903999efa1",
+    "url": "/typing-react/static/js/2.93b04960.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typing-react/static/js/2.93b04960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c2f32c9f56f18c6854b",
+    "url": "/typing-react/static/js/main.1bef6714.chunk.js"
+  },
+  {
+    "revision": "de7052e32d48907f5de6",
+    "url": "/typing-react/static/js/runtime-main.3f88511f.js"
+  }
+]);
